442ed4bbe57799de3dd596208edf6dd74a997117
Rhouma-Ghada <ghada.rhouma.123@gmail.com>
2020-11-10 00:10:23 +0100

    ajout des fontions dans main ativity et Second activity + des log pour répendre au question demandé

--- app/src/main/java/com/example/hellotost/MainActivity.java
+++ app/src/main/java/com/example/hellotost/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,12 +13,27 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount =0;
     private TextView mShowCount;
+    //définir la clé de l'extra de l'intention
+    public static final String EXTRA_MESSAGE =
+            "com.example.hellotost.extra.MESSAGE";
+
+    private TextView mMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        //*******
+        mMessageTextView= findViewById(R.id.show_count);
+        Log.d("qest1", "3");
+        Log.d("qest2", "1;3");
+        Log.d("qest3", "2");
+        Log.d("qest4", "4");
+        Log.d("quest5", "4");
+
+
+
     }
 /*
     public void showToast(View view) {
@@ -33,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageTextView.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
